0f5d51211dfdb1dc461dfa38ce08d75cd9cba3a1
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-07 13:00:36 -0600

    hm trouble

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import 'jquery'
+import 'popper.js'
+import 'css/site'
 
 Rails.start()
 Turbolinks.start()
