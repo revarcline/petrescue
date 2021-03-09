1ef43806610e0fff79638bb95ffc8f0183e530fb
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-09 11:48:00 -0600

    lol

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,1 @@
-OmniAuth.config.full_host = Rails.env.production? ? 'https://revarcline-petrescue.herokuapp.com/' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://revarcline-petrescue.herokuapp.com' : 'http://localhost:3000'
