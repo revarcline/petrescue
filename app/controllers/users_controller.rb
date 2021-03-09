4d754662ace6089dc01f429d54a5eb2a28ca3b69
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-09 13:32:19 -0600

    user index for admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :admin_only, only: :index
+
+  def index
+    @users = User.all
+  end
+
   def show
     set_user
+    if @user == current_user || current_user.admin
+      render :show
+    else
+      flash[:notice] = 'You are not allowed to see that page'
+      redirect_to root_path
+    end
   end
 
   private
