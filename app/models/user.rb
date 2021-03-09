e9c630eed055f8d3c57118afa0106d09e4d5a7d8
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-09 11:40:17 -0600

    oauth is working now mostly

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,18 @@ class User < ApplicationRecord
   has_many :appointments
   has_many :pets, through: :appointments
 
-def self.from_omniauth(access_token)
+  def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
-        user = User.create(full_name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
-        )
+      user = User.create(full_name: data['name'],
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
     end
     user
-end
+  end
 
 end
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,1 @@
+OmniAuth.config.full_host = Rails.env.production? ? 'https://revarcline-petrescue.herokuapp.com/' : 'http://localhost:3000'
