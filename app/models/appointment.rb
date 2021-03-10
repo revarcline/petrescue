3bc0434de4aefa1f9dda9f2406b61921d34340fd
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-10 13:48:08 -0600

    validation, using many to many more

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,10 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :pet
 
-  validates :start, inclusion: { in: (Date.current..Date.current+6.months) }
-
+  validates :start, inclusion: { in: (Date.current+1.day..Date.current+6.months) }
+  validates_uniqueness_of :start, scope: :pet
+  
+  def self.upcoming
+    Appointment.where('start > ?', DateTime.current)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,4 @@ class User < ApplicationRecord
     end
     user
   end
-
 end
