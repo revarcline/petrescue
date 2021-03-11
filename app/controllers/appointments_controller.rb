6df48eba291d7219d2c9dc59cb1a57779e08ff48
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-11 16:56:50 -0600

    fixed appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,52 +1,54 @@
 class AppointmentsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :admin_only, only: :index
+  before_action :authenticate_user!
+  before_action :admin_only, only: :index
 
-    def new
-        @appointment = Appointment.new
-        set_pet
-    end
-    
-    def index
-        @appointments = Appointment.all
-    end
+  def new
+    @appointment = Appointment.new
+    set_pet
+  end
 
-    def create
-        @appointment = Appointment.new(appointment_params)
-        set_pet
-        @appointment.pet = @pet
-        @appointment.user = current_user
-        if @appointment.save
-            redirect_to pet_appointment_path(@appointment.pet, @appointment)
-        else
-            render :new
-        end
-    end
+  def index
+    @appointments = Appointment.all
+  end
 
-    def show
-        set_appointment
+  def create
+    @appointment = Appointment.new(appointment_params)
+    set_pet
+    @appointment.pet = @pet
+    @appointment.user = current_user
+    if @appointment.save
+      redirect_to pet_appointment_path(@appointment.pet, @appointment)
+    else
+      render :new
     end
+  end
 
-    def destroy
-        set_appointment
-        if current_user == @appointment.user || current_user.admin
-        else
-            params[:notification] = "You are not allowed to delete that appointment"
-        end
-        redirect_to root
-    end
-    
-    private
+  def show
+    set_appointment
+  end
 
-    def set_appointment
-        @appointment = Appointment.find_by_id(params[:id])
+  def destroy
+    set_appointment
+    if current_user == @appointment.user || current_user.admin
+      @appointment.destroy
+      params[:notice] = 'Appointment deleted successfully'
+    else
+      params[:notice] = 'You are not allowed to delete that appointment'
     end
+    redirect_to user_path(current_user)
+  end
 
-    def set_pet
-        @pet = Pet.find_by_id(params[:pet_id])
-    end
+  private
 
-    def appointment_params
-        params.require(:appointment).permit(:timeslot)
-    end
+  def set_appointment
+    @appointment = Appointment.find_by_id(params[:id])
+  end
+
+  def set_pet
+    @pet = Pet.find_by_id(params[:pet_id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:timeslot)
+  end
 end
